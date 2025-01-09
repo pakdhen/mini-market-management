f229504649cf89079a632d0734c2bf1319ded95b
erikanaz <erikanazwa222@gmail.com>
2025-01-09 21:43:29 +0700

    menambahkan route untuk navigasi

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BranchController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use App\Models\Transaction;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,7 +21,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('branches', BranchController::class);
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
+    Route::get('/stocks', [StockController::class, 'index'])->name('stocks');
 });
 
-require __DIR__.'/auth.php';
+Route::group(['middleware' => ['role:Owner']], function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+    Route::get('/branches', [BranchController::class, 'index'])->name('branches');
+});
+
+Route::middleware(['auth', 'role:Owner|Manager|Supervisor|Kasir'])->group(function () {
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions');
+});
+
+require __DIR__ . '/auth.php';
