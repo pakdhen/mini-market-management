b34a641bcd2c4fef53bc122ca74b6c698c7a61ea
silvaaulia <silvaaulia190@gmail.com>
2025-01-10 12:10:40 +0700

    Menambahkan tampilan untuk data cabang/branches

--- resources/views/branches/create.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('branches.store') }}" method="POST">
-    @csrf
-    <label for="name">Branch Name:</label>
-    <input type="text" name="name" required>
-    <label for="address">Address:</label>
-    <input type="text" name="address" required>
-    <button type="submit">Add Branch</button>
-</form>
